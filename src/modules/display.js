b309b8cbfe9d25a3d5287dad7aba3f922e3c6cce
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-18 13:31:10 -0300

    Add like button function

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,3 +1,43 @@
+const likeShow = (array) => {
+  const myArray = array.filter((object) => object.item_id !== '');
+  console.log(myArray);
+  myArray.forEach((object) => {
+    const likes = document.querySelector(`#count${object.item_id}`);
+    if (object.likes === null) {
+      likes.innerHTML = '0 Likes';
+    } else {
+      likes.innerHTML = `${object.likes} Likes`;
+    }
+  });
+};
+
+const getLike = async () => {
+  await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/TUbnIqBO6HOTZf6v0kyK/likes/', {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => likeShow(json));
+};
+
+window.addEventListener('load', () => {
+  getLike();
+});
+
+const likeSend = async (id) => {
+  await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/TUbnIqBO6HOTZf6v0kyK/likes/', {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: id,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+};
+
 const display = (array) => {
   const container = document.querySelector('#maincontainer');
   array.forEach((item) => {
@@ -6,16 +46,24 @@ const display = (array) => {
     <div class="nameandlikes">
     <h2 class="mealname">${item.strMeal}</h2>
     <div class="likesdiv">
-    <button class="likebutton" type="button">
+    <button id="like${item.idMeal}" class="likebutton" type="button">
     <ion-icon class="heart" name="heart-outline"></ion-icon>
     </button>
-    <p>Likes</p>
+    <p id="countlike${item.idMeal}"></p>
     </div>
     </div>
     <div class="buttonholder">
     <button class="comments button" id=${item.idMeal} type="button">Comments</button>
     </div>
     </section>`;
+  });
+  const likebuttons = document.querySelectorAll('.likebutton');
+  likebuttons.forEach((button) => {
+    button.addEventListener('click', async (e) => {
+      const identity = e.target.parentNode.id;
+      await likeSend(identity);
+      getLike();
+    });
   });
 };
 
