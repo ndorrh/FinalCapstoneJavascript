cba9156a4c2c818c8daa5a4a8fe6f881f7adb52c
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-18 11:56:26 -0300

    Fix style issues, add like button placeholders

--- src/modules/display.js
+++ src/modules/display.js
@@ -3,7 +3,15 @@ const display = (array) => {
   array.forEach((item) => {
     container.innerHTML += `<section class="section">
     <img class="mainimage" src=${item.strMealThumb} alt=${item.strMeal}>
-    <h2>${item.strMeal}</h2>
+    <div class="nameandlikes">
+    <h2 class="mealname">${item.strMeal}</h2>
+    <div class="likesdiv">
+    <button class="likebutton" type="button">
+    <ion-icon class="heart" name="heart-outline"></ion-icon>
+    </button>
+    <p>Likes</p>
+    </div>
+    </div>
     <div class="buttonholder">
     <button class="comments button" id=${item.idMeal} type="button">Comments</button>
     </div>
