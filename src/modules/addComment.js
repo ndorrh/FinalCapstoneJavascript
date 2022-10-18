3c59519c87aef52270d5d20d755e142534290b77
ndorrh <ndorrh@gmail.com>
2022-10-18 19:16:27 +0100

    Add function to send comment to involment API and retrive it using the getDataFromInvolvementApi() functiom

---
+++ src/modules/addComment.js
@@ -0,0 +1,37 @@
+const commentBtn = document.getElementById('commentBtn');
+const previousComment = document.getElementById('prev-comments');
+const userName = document.getElementById('name');
+const comment = document.getElementById('comment');
+
+const getDataFromInvolvementApi = async () => {
+  await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/TUbnIqBO6HOTZf6v0kyK/comments?item_id=15')
+    .then((rawData) => rawData.json())
+    .then((commentData) => {
+      previousComment.innerHTML = '';
+      commentData.forEach((message, index) => {
+        previousComment.innerHTML += `<li class="comment-prev">${commentData[index].creation_date} ${commentData[index].username}: ${commentData[index].comment} </li>`;
+      });
+    });
+};
+
+const sendData = async () => {
+  const data = {
+    item_id: 15,
+    username: userName.value,
+    comment: comment.value,
+  };
+
+  await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/TUbnIqBO6HOTZf6v0kyK/comments/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  getDataFromInvolvementApi();
+
+  userName.value = '';
+  comment.value = '';
+};
+
