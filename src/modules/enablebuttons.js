e43e3e909e889512a095bb58082c4923e929a765
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-18 19:06:09 -0300

    Style like counters

--- src/modules/enablebuttons.js
+++ src/modules/enablebuttons.js
@@ -7,6 +7,8 @@ const enableButton = () => {
   likebuttons.forEach((button) => {
     button.addEventListener('click', async (e) => {
       const heart = e.target.querySelector('.heart');
+      const likecount = document.querySelector(`#count${e.target.id}`)
+      likecount.style.color = 'rgb(253, 65, 65)';
       heart.setAttribute('name', 'heart');
       button.classList.add('pressed');
       await likeSend(e.target.id);
