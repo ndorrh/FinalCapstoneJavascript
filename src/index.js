3a5cd4ef5d3c61fb575576c0b4b3ef96bca20ab2
ndorrh <ndorrh@gmail.com>
2022-10-18 21:29:58 +0100

    Add event listerner to close button on commentPop up

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import fetcher from './modules/fetcher.js';
 
 import './modules/addComment.js';
 
-
-
 window.addEventListener('load', () => {
   fetcher();
 });
--- src/modules/addComment.js
+++ src/modules/addComment.js
@@ -1,9 +1,15 @@
 import { getData } from './commentPopUp.js';
 
-const commentBtn = document.getElementById('commentBtn');
+const commentWrapper = document.getElementById('comment-popup');
 const previousComment = document.getElementById('prev-comments');
 const userName = document.getElementById('name');
 const comment = document.getElementById('comment');
+const commentPopUpContainer = document.getElementById('body');
+const mealsDatails = document.getElementById('meal-details');
+const maincontainer = document.getElementById('maincontainer');
+const header = document.getElementById('header');
+const footer = document.getElementById('footer');
+const closeBtn = document.getElementById('closeBtn');
 
 const getDataFromInvolvementApi = async (mealId) => {
   await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/TUbnIqBO6HOTZf6v0kyK/comments?item_id=${mealId}`)
@@ -37,14 +43,16 @@ const sendData = async (mealId) => {
   comment.value = '';
 };
 
-
-const commentPopUpContainer = document.getElementById('body');
-const mealsDatails = document.getElementById('meal-details');
-
 let id = 0;
+// Add event listener to booth Add comment button and comment button on home page
 commentPopUpContainer.addEventListener('click', (e) => {
   const btnId = parseInt(e.target.id, 10);
   if (e.target.classList.contains('comments')) {
+    commentWrapper.classList.remove('hiden');
+    commentWrapper.style.display = 'flex';
+    maincontainer.style.display = 'none';
+    header.style.display = 'none';
+    footer.style.display = 'none';
     getDataFromInvolvementApi(btnId);
     mealsDatails.innerHTML = '';
     getData(btnId);
@@ -56,9 +64,10 @@ commentPopUpContainer.addEventListener('click', (e) => {
   }
 });
 
-
-
-//commentBtn.addEventListener('click', (e) => {
-  //e.preventDefault();
- 
-//});
+// Adding Event listener to close button on comment Popup
+closeBtn.addEventListener('click', () => {
+  commentWrapper.classList.add('hiden');
+  maincontainer.style.display = 'flex';
+  header.style.display = 'flex';
+  footer.style.display = 'flex';
+});
