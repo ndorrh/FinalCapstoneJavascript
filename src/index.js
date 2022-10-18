b961a8bb047ff394f0ed35bcce5414e8f29ec1b6
ndorrh <ndorrh@gmail.com>
2022-10-18 06:41:39 +0100

    Call render function in index.js to display dinamically generated elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+import { render, commentPopUp } from './modules/commentPopUp.js';
+
+commentPopUp.innerHTML = render();
+
+const comment = document.getElementById('category');
+
+console.log(comment);
