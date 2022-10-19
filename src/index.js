a1a20a66c518cc863f077f3411a9904d80000720
ndorrh <ndorrh@gmail.com>
2022-10-19 06:20:40 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import './style.css';
-
 import fetcher from './modules/fetcher.js';
-
-
 import getLike from './modules/getlike.js';
+import './modules/addComment.js';
 
 window.addEventListener('load', () => {
   fetcher();
   getLike();
 });
 
-import './modules/addComment.js';
-
 window.addEventListener('load', () => {
   fetcher();
 });
-
