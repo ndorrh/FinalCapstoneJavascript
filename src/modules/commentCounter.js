e01b66c8f121c4d86c8eaf361e248028cbe36901
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-19 12:19:56 -0300

    Remove console statement

--- src/modules/commentCounter.js
+++ src/modules/commentCounter.js
@@ -1,7 +1,6 @@
 const counter = document.getElementById('no-of-comments');
 const commentCounter = (arrOfComments) => {
   counter.innerHTML = `(${arrOfComments.length})`;
-  console.log(counter);
 };
 
 export { counter, commentCounter };
