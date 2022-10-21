ca7f6d615cab48f0f183f13308245623a8549fd6
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-21 15:31:35 -0300

    Make suggested fixes

--- src/modules/likeshow.js
+++ src/modules/likeshow.js
@@ -2,11 +2,7 @@ const likeShow = (array) => {
   const myArray = array.filter((object) => object.item_id !== '');
   myArray.forEach((object) => {
     const likes = document.querySelector(`#count${object.item_id}`);
-    if (object.likes === null) {
-      likes.innerHTML = '0 Likes';
-    } else {
-      likes.innerHTML = `${object.likes} Likes`;
-    }
+    likes.innerHTML = (object.likes === null) ? '0 Likes' : `${object.likes} Likes`;
   });
 };
 
